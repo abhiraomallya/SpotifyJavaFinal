e1626cc0837382cc79f7335ace29bbb7734d69f8
Ron Rizzo <churchmouth13@gmail.com>
2023-04-20 11:28:34 -0400

    Port React front end into existing app
    This commit creates a React app in the frontend/ui folder, and ports
    code over from test React app. Correctly displays frontend by running
    npm start in ui folder. Need to fix interaction with back end.

---
+++ src/main/frontend/ui/src/App.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import ArtistList from "./Components/ArtistList";
+import uniqid from "uniqid";
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      artist: { 
+        name: '',
+        id: uniqid(),
+        rank: 0
+      },
+      topArtists: [],
+    };
+  }
+
+  getTopArtists = async (e) => {
+    e.preventDefault();
+    const artistList = await fetch('http://localhost:8888/top-artists')
+      .then(function(response){
+        return response.json()
+      })
+      .catch(function(err) {
+        console.error(err);
+      })
+    artistList.forEach((entry, index) => {
+      this.setState({
+        artist: {
+          name: entry,
+          rank: index + 1,
+          id: uniqid()
+        },
+        topArtist: this.state.topArtists.concat(this.state.artist)
+      })
+    })
+    }
+  
+
+  render(){
+    const { artist, topArtists } = this.state;
+
+    return (
+      <div>
+        <h1>Spotify Top Artists</h1>
+        <button 
+          className='getBtn'
+          onClick={this.getTopArtists}
+        >
+          Get Top Artists
+        </button>
+        <ArtistList topArtists={topArtists}></ArtistList>
+      </div>
+    );
+  }
+}
+
+export default App;
