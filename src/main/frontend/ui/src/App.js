efd7dff1ad128ee1897f702a28b91a8dc067d565
Ron Rizzo <churchmouth13@gmail.com>
2023-04-24 15:56:46 -0400

    Update frontend to print requested info to console
    Tried to simplify the frontend to debug. Seems to correctly make fetch
    request once before returning an error related to cors. I've tried a
    few solutions, at this point my theory is that it has something to do
    with how the backend is handling the fetch requests.

--- src/main/frontend/ui/src/App.js
+++ src/main/frontend/ui/src/App.js
@@ -16,17 +16,27 @@ class App extends Component {
       topArtists: [],
     };
   }
-
+/**
+ * Currently fetches top-artists from backend and logs to console. Seems to only work once
+ * before throwing this error: 
+ * Access to fetch at 'http://localhost:8888/top-artists' from origin 'http://localhost:3000' 
+ * has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on 
+ * the requested resource. If an opaque response serves your needs, set the request's mode 
+ * to 'no-cors' to fetch the resource with CORS disabled.
+ */
   getTopArtists = async (e) => {
     e.preventDefault();
     const artistList = await fetch('http://localhost:8888/top-artists', { mode: 'cors' })
       .then(function(response){
-        return response.json()
+        return response.json();
+      })
+      .then(function(response) {
+        console.log(response);
       })
       .catch(function(err) {
         console.error(err);
       })
-    artistList.forEach((entry, index) => {
+    /*artistList.forEach((entry, index) => {
       this.setState({
         artist: {
           name: entry,
@@ -35,7 +45,7 @@ class App extends Component {
         },
         topArtist: this.state.topArtists.concat(this.state.artist)
       })
-    })
+    })*/
     }
   
 
