569849035de761a3481cda125725bcc4a5d8f423
Ron Rizzo <churchmouth13@gmail.com>
2023-04-30 19:33:00 -0400

    Begin modifying front end for final design
    Edits to frontend to start moving towards final design. Using drop down
    menus to control calls to backend, situated on a nav bar at the top of
    the page

--- src/main/frontend/app.js
+++ src/main/frontend/app.js
@@ -24,26 +24,26 @@ function displayArtists(artists, artistListId) {
     });
 }
 
-document.getElementById('fetch-artists').addEventListener('click', async () => {
+document.getElementById('artists-medium').addEventListener('click', async () => {
     await fetchAndDisplayAPIData('http://localhost:8888/top-artists', 'artist-list');
 });
 
-document.getElementById('fetch-artists-long').addEventListener('click', async () => {
+document.getElementById('artists-long').addEventListener('click', async () => {
     await fetchAndDisplayAPIData('http://localhost:8888/top-artists-long', 'artist-list-long');
 });
 
-document.getElementById('fetch-artists-short').addEventListener('click', async () => {
+document.getElementById('artists-short').addEventListener('click', async () => {
     await fetchAndDisplayAPIData('http://localhost:8888/top-artists-short', 'artist-list-short');
 });
 
-document.getElementById('fetch-tracks').addEventListener('click', async () => {
+document.getElementById('tracks-medium').addEventListener('click', async () => {
     await fetchAndDisplayAPIData('http://localhost:8888/top-tracks', 'track-list');
 });
 
-document.getElementById('fetch-tracks-long').addEventListener('click', async () => {
+document.getElementById('tracks-long').addEventListener('click', async () => {
     await fetchAndDisplayAPIData('http://localhost:8888/top-tracks-long', 'track-list-long');
 });
 
-document.getElementById('fetch-tracks-short').addEventListener('click', async () => {
+document.getElementById('tracks-short').addEventListener('click', async () => {
     await fetchAndDisplayAPIData('http://localhost:8888/top-tracks-short', 'track-list-short');
 });
