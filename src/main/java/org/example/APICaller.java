eefd376b4912d70cf1bc7613ad31a4dcd0c9c21a
lonebrow <88587287+lonebrow@users.noreply.github.com>
2023-05-04 13:52:03 -0400

    Add files via upload

--- src/main/java/org/example/APICaller.java
+++ src/main/java/org/example/APICaller.java
@@ -4,6 +4,6 @@ public class APICaller {
 	public static void main(String[] args) throws Exception {
 		//starts server on port 8888
 		Server.startServer();
-
+		System.out.println("The frontend is hosted here: \nhttps://getlucky13.github.io/SpotifyStats/");
 	}
 }
